2552a80b931f2d11edcec82f53faf2ac48b4da0c
Jordan <jordan123@gmail.com>
2011-09-24 16:14:29 -0600

    spell error

--- app/controllers/winners_controller.rb
+++ app/controllers/winners_controller.rb
@@ -1,6 +1,6 @@
 class WinnersController < ApplicationController
   def index
-    @title = "Winnders"
+    @title = "Winners"
     #@girls = Girl.order('score DESC').limit(100).all
     @girls = Girl.paginate(:page => params[:girls_page]).limit(5).order('score DESC')
     @boys = Guys.paginate(:page => params[:boys_page]).limit(5).order('score DESC')
