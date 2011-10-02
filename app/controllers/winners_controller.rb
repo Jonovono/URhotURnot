8a048d8b0dcca80427213d3f7bfaa761fc16dbfb
Jordan <jordan123@gmail.com>
2011-10-01 23:37:17 -0600

    change basck

--- app/controllers/winners_controller.rb
+++ app/controllers/winners_controller.rb
@@ -2,11 +2,11 @@ class WinnersController < ApplicationController
   def index
     @title = "Winners"
     #@girls = Girl.order('score DESC').limit(100).all
-    #@girls = Girl.paginate(:page => params[:girls_page]).limit(5).order('score DESC')
-    @girls = Girl.paginate(:page => params[:girls_page]).where('girls.username IN(SELECT username FROM girls ORDER BY score DESC LIMIT 100)').per_page(5)
-    #@boys = Guys.paginate(:page => params[:boys_page]).limit(5).order('score DESC')
+    @girls = Girl.paginate(:page => params[:girls_page]).limit(5).order('score DESC')
+    #@girls = Girl.paginate(:page => params[:girls_page]).where('girls.username IN(SELECT username FROM girls LIMIT 100)').order('score DESC').per_page(5)
+    @boys = Guys.paginate(:page => params[:boys_page]).limit(5).order('score DESC')
     #@girls = Girl.paginate(:page => params[:girls_page])
-    @boys = Guys.paginate(:page => params[:boys_page]).where('guys.username IN(SELECT username FROM guys ORDER BY score DESC LIMIT 100)').per_page(5)
+    #@boys = Guys.paginate(:page => params[:boys_page]).order('score DESC')where('guys.username IN(SELECT username FROM guys LIMIT 100)').per_page(5)
   end
 
 end
