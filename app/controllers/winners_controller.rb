117bb680e6e485db8c8074b2d10b24f05e039b8b
Jordan <jordan123@gmail.com>
2011-10-01 23:27:06 -0600

    top 100

--- app/controllers/winners_controller.rb
+++ app/controllers/winners_controller.rb
@@ -2,9 +2,11 @@ class WinnersController < ApplicationController
   def index
     @title = "Winners"
     #@girls = Girl.order('score DESC').limit(100).all
-    @girls = Girl.paginate(:page => params[:girls_page]).limit(5).order('score DESC')
-    @boys = Guys.paginate(:page => params[:boys_page]).limit(5).order('score DESC')
+    #@girls = Girl.paginate(:page => params[:girls_page]).limit(5).order('score DESC')
+    @girls = Girl.paginate(:page => params[:girls_page]).where('girls.username IN(SELECT username FROM girls ORDER BY score LIMIT 100)').per_page(5)
+    #@boys = Guys.paginate(:page => params[:boys_page]).limit(5).order('score DESC')
     #@girls = Girl.paginate(:page => params[:girls_page])
+    @boys = Guys.paginate(:page => params[:boys_page]).where('guys.username IN(SELECT username FROM guys ORDER BY score LIMIT 100)').per_page(5)
   end
 
 end
